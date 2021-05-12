70a52aecddbc2def1478c640fdd06fd373507c78
Dilberovicka31 <dilberovicm36@gmail.com>
2021-05-11 23:38:46 -0400

    starting files plus google maps

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import GoogleMaps from "./components/googlemaps";
+
+function App() {
+  return (
+    <div className="App">
+     <GoogleMaps/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/googlemaps/index.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { GoogleMap, LoadScript } from '@react-google-maps/api';
+
+const containerStyle = {
+  width: '400px',
+  height: '400px'
+};
+
+const center = {
+  lat: 46.2621250486355,
+  lng: -119.25413566961782
+
+  
+};
+
+function GoogleMaps() {
+  return (
+    <LoadScript
+      googleMapsApiKey="AIzaSyARqAQsa7oxBn9RDMFtz6GHO8-KVMfz7hQ"
+    >
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={center}
+        zoom={10}
+      >
+        { /* Child components, such as markers, info windows, etc. */ }
+        <></>
+      </GoogleMap>
+    </LoadScript>
+  )
+}
+
+export default React.memo(GoogleMaps)
